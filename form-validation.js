810eba85f595be66603a1620c652714f2ed212a0
Kevin Buffardi <kbuffardi@csuchico.edu>
2016-10-09 19:57:02 -0700

    Initial code for students to start validation lab

---
+++ form-validation.js
@@ -0,0 +1,19 @@
+function validate(){
+  //Grab the user's input
+  var passEntered = document.getElementById("pass").value;
+  var userEntered = document.getElementById("user").value;
+
+  //Show message that there is an error with the username...
+  document.getElementById("usernameError").innerHTML="Bad username.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+  //Turn the username items red
+  document.getElementById("usernameGroup").classList.add("has-error");
+
+  //Show message that there is an error with the password...
+  document.getElementById("passwordError").innerHTML="Bad password.";
+  document.getElementById("passwordError").classList.remove("hidden-message");
+  document.getElementById("passwordError").classList.add("shown-message");
+  //Turn the password items red
+  document.getElementById("passwordGroup").classList.add("has-error");
+}
